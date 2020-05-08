f0c1e8586c061b325f16b2a49cf5726871d700a9
Jantzen-E <jantzen.egan@gmail.com>
2020-05-08 00:48:16 -0600

    animations initial commit

---
+++ animations/controls.js
@@ -0,0 +1,35 @@
+$(document).ready(function() {
+    
+    //variables
+    $surface = $('.surface');
+    $car = $('.car');
+    $img = $('.car img');
+    $nightSky = $('.night')
+    let flag = true;
+
+    const cars = ['./assets/Img_05.png', './assets/Img_06.png'];
+
+    //keypress event
+    //vehicle moves with the "enter" keypress
+    $(document).on("keypress", function(e) {
+        if (e.which == 13) {
+            $($surface).toggleClass('moveRight');
+            $($car).toggleClass('suspension');
+            $($nightSky).toggleClass('nightSky');
+        }
+    })
+
+    //lights turn on and off with the "w" keypress
+    $(document).on("keypress", function(e) {
+        if (e.which == 119) {
+            if (flag) {
+                flag = false;
+                $img.attr('src', cars[0])
+            }
+            else {
+                flag = true;
+                $img.attr('src', cars[1])
+            }
+        }
+    })
+});
