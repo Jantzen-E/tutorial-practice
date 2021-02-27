31f3cbdd6fce92b1b4277ad3f5543b87fe536d8f
Jantzen-E <jantzen.egan@gmail.com>
2021-02-26 19:31:11 -0700

    implement the logout feature

--- auth-demo/index.js
+++ auth-demo/index.js
@@ -22,6 +22,13 @@ app.set('views', 'views');
 app.use(express.urlencoded({ extended: true }));
 app.use(session({ secret: 'notagoodsecret' }));
 
+const requireLogin = (req, res, next) => {
+    if (!req.session.user_id) {
+        return res.redirect('/login');
+    }
+    next();
+}
+
 app.get('/', (req, res) => {
     res.send('this is the home page')
 })
@@ -35,9 +42,9 @@ app.post('/register', async (req, res) => {
     // res.send(req.body)
     const { password, username } = req.body;
     const hash = await bcrypt.hash(password, 12);
-    const user = new User({
-        username,
-        password: hash
+    const user = new User({ 
+        username, 
+        password: hash 
     })
     await user.save();
     req.session.user_id = user._id;
@@ -49,25 +56,39 @@ app.post('/register', async (req, res) => {
 app.get('/login', (req, res) => {
     res.render('login')
 })
+
 app.post('/login', async (req, res) => {
-    // res.send(req.body)
+    res.send(req.body)
     const {username, password} = req.body;
+    // const foundUser = await User.authenticate(username, password);
     const user = await User.findOne({ username });
-    const validPassword = await bcrypt.compare(password, user.password);
+    const validPassword = await bcrypt.compare(req.body.password, user.password);
     if (validPassword) {
         req.session.user_id = user._id;
         res.redirect('/secret');
     }
     else {
         res.redirect('/login')
+        res.send('Please try to log in again')
+        .catch(error => {throw error})
     }
 })
 
-app.get('/secret', (req, res) => {
-    if (!req.session.user_id) {
-        res.redirect('/login')
-    }
-    res.send('This is secret! you cannot see me unless you are logged in')
+app.post('/logout', (req, res) => {
+    req.session.user_id = null;
+    // req.session.destroy();
+    res.redirect('/login');
+})
+
+app.get('/secret', requireLogin, (req, res) => {
+    // if (!req.session.user_id) {
+    //     return res.redirect('/login')
+    // }
+    res.render('secret')
+})
+
+app.get('/topsecret', requireLogin, (req, res) => {
+    res.send('top secret')
 })
 
 app.listen(3000, () => {
