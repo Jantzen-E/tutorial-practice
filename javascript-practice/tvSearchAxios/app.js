5cf00d00d77ce79d3cc14c04a0e7c436275ba556
Jantzen-E <jantzen.egan@gmail.com>
2021-03-21 22:01:18 -0600

    tv search practice project with axios

---
+++ javascript-practice/tvSearchAxios/app.js
@@ -0,0 +1,22 @@
+const form = document.querySelector('#searchForm');
+form.addEventListener('submit', async function(e) {
+    e.preventDefault();
+    // console.log('submitted!')
+    // console.log(form.elements.query.value)
+    const searchTerm = form.elements.query.value;
+    const config = { params: {q: searchTerm } }
+    const res = await axios.get(`http://api.tvmaze.com/search/shows`, config);
+    // console.log(res.data[0].show.image.medium);
+    displayImages(res.data)
+    form.elements.query.value = '';
+})
+
+const displayImages = (shows) => {
+    for (let result of shows) {
+        if(result.show.image) {
+            const img = document.createElement('IMG');
+            img.src = result.show.image.medium;
+            document.body.append(img);
+        }
+    }
+}
