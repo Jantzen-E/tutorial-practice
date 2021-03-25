4b0ad82e74c54397061cdf0549ca340a5a18b1e4
Jantzen-E <jantzen.egan@gmail.com>
2021-03-24 22:43:01 -0600

    use the 'this' keyword with factory functions

--- javascript-practice/factory-functions/app.js
+++ javascript-practice/factory-functions/app.js
@@ -12,9 +12,18 @@ function makeColor(r, g, b) {
     color.r = r;
     color.g = g;
     color.b = b;
-    // color.rgb = function()
+    color.rgb = function() {
+        const {r, g, b} = this;
+        // console.log(this);
+        return `rgb(${r}, ${g}, ${b})`;
+    };
+    color.hex = function() {
+        const {r, g, b} = this;
+        return '#' + ((1 << 24) + (r << 16) + (g << 8) + b).toString(16).slice(1);
+    }
     return color;
 }
 
+const firstColor = makeColor(35, 255, 150);
 // hex(255, 100, 25);
 // '#ff6419'
