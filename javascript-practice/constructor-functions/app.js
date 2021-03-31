62dfc6b765f1d64e42c3fe1a22fe0dbfc4309ebf
Jantzen-E <jantzen.egan@gmail.com>
2021-03-30 22:52:58 -0600

    javascript class basics

--- javascript-practice/constructor-functions/app.js
+++ javascript-practice/constructor-functions/app.js
@@ -45,17 +45,20 @@ class Color {
     // greet() {
     //     return `hello from ${this.name}!`;
     // }
-    rgb() {
+    innerRGB() {
         const {r, g, b} = this;
-        return `rgb(${r}, ${g}, ${b})`;
+        return `${r}, ${g}, ${b}`;
+    }
+    rgb() {
+        return `rgb(${this.innerRGB()})`;
     }
     hex() {
         const {r, g, b} = this;
         return '#' + ((1 << 24) + (r << 16) + (g << 8) + b).toString(16).slice(1);
     }
     rgba(a = 1.0) {
-        const {r, g, b} = this;
-        return `rgba(${r}, ${g}, ${b}, ${a})`;
+        // const {r, g, b} = this;
+        return `rgba(${this.innerRGB()}, ${a})`;
     }
 }
 
