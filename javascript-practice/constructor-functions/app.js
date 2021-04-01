f5bf3a3b92bf972bfa0f7d5dd81366dad1f2be01
Jantzen-E <jantzen.egan@gmail.com>
2021-03-31 23:34:52 -0600

    add hsl function to convert rgb to hsl colors

--- javascript-practice/constructor-functions/app.js
+++ javascript-practice/constructor-functions/app.js
@@ -41,10 +41,8 @@ class Color {
         this.g = g;
         this.b = b;
         this.name = name;
+        this.calcHSL();
     }
-    // greet() {
-    //     return `hello from ${this.name}!`;
-    // }
     innerRGB() {
         const {r, g, b} = this;
         return `${r}, ${g}, ${b}`;
@@ -57,8 +55,45 @@ class Color {
         return '#' + ((1 << 24) + (r << 16) + (g << 8) + b).toString(16).slice(1);
     }
     rgba(a = 1.0) {
-        // const {r, g, b} = this;
         return `rgba(${this.innerRGB()}, ${a})`;
+    }
+    hsl() {
+        const {h, s, l} = this;
+        return `hsl(${h}, ${s}%, ${l}%)`
+    }
+    calcHSL() {
+        let {r, g, b} = this;
+        r /= 255;
+        g /= 255;
+        b /= 255;
+    
+        let cmin = Math.min(r, g, b),
+            cmax = Math.max(r, g, b),
+            delta = cmax - cmin,
+            h = 0,
+            s = 0,
+            l = 0;
+        if (delta == 0) h = 0;
+        else if (cmax == r)
+            h = ((g - b) / delta) % 6;
+        else if (cmax == g)
+            h = (b - r) / delta + 2;
+        else 
+           h = (r - g) / delta + 4;
+    
+        h = Math.round(h + 60);
+    
+        if (h < 0) h += 360;
+    
+        l = (cmax + cmin) / 2;
+    
+        s = delta == 0 ? 0 : delta / (1 - Math.abs(2 * l - 1));
+    
+        s = +(s * 100).toFixed(1);
+        l = +(l * 100).toFixed(1);
+        this.h = h;
+        this.s = s;
+        this.l = l;
     }
 }
 
