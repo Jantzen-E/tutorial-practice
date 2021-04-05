98c7c21fc80d8c96028d7de92a21f3619a90dbe8
Jantzen-E <jantzen.egan@gmail.com>
2021-04-04 21:52:09 -0600

    learn about inheritance using the super keyword

--- javascript-practice/extendsAndSupers/app.js
+++ javascript-practice/extendsAndSupers/app.js
@@ -1,5 +1,12 @@
+//there is a method called eat on the pet class and on the dog class
+//if you console.log the name of a dog that is defined then .eat(), it will return the eat method from the dog
+//class instead of the pet class because of the order that it works
+//it checks to see in the dog class if there is an eat method and if not, it goes up and finds the 
+//eat method on the Pet class where the constructor is because the dog class uses the keyword 'extends'
+
 class Pet {
     constructor(name, age) {
+        console.log('in pet constructor')
         this.name = name;
         this.age = age;
     }
@@ -8,7 +15,14 @@ class Pet {
     }
 }
 
+//the keyword super means that it uses the base Pet class name and age from where it is extending from
+//that uses concepts of inheritance because using super inherits the name and age from the base class 'Pet'
 class Cat extends Pet {
+    constructor(name, age, livesLeft = 9) {
+        console.log('in cat constructor')
+        super(name, age)
+        this.livesLeft = livesLeft;
+    }
     meow() {
         return 'Meowwwwwww';
     }
@@ -17,5 +31,8 @@ class Cat extends Pet {
 class Dog extends Pet {
     bark() {
         return 'ruff ruff';
+    }
+    eat() {
+        return `${this.name} scarfs his food!`
     }
 }
