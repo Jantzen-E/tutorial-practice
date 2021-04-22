a1a994fd1bd694c838cfc0c83b1b5c7085f1eaee
Jantzen-E <jantzen.egan@gmail.com>
2021-04-21 23:56:04 -0600

    practice using the request and response objects in express

--- express/firstApp/index.js
+++ express/firstApp/index.js
@@ -2,8 +2,11 @@ const express = require("express");
 const app = express();
 // console.dir(app);
 
-app.use(() => {
+app.use((req, res) => {
     console.log("we got a new request")
+    // res.send("hello, we got your request")
+    // res.send({ color: 'red' });
+    res.send('<h1>This is my webpage!</h1>');
 })
 
 app.listen(5000, () => {
