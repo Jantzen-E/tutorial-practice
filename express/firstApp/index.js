2f0cd36f373f3964b560c4c6ea4d280e56195d47
Jantzen-E <jantzen.egan@gmail.com>
2021-05-06 23:05:16 -0600

    routing with express.js

--- express/firstApp/index.js
+++ express/firstApp/index.js
@@ -2,11 +2,27 @@ const express = require("express");
 const app = express();
 // console.dir(app);
 
-app.use((req, res) => {
-    console.log("we got a new request")
-    // res.send("hello, we got your request")
-    // res.send({ color: 'red' });
-    res.send('<h1>This is my webpage!</h1>');
+// app.use((req, res) => {
+//     console.log("we got a new request")
+//     res.send("hello, we got your request")
+//     res.send({ color: 'red' });
+//     res.send('<h1>This is my webpage!</h1>');
+// })
+
+app.get('/', (req, res) => {
+    res.send('this is the home page')
+})
+
+app.get('/cats', (req, res) => {
+    res.send('meow')
+})
+
+app.post('/cats', (req, res) => {
+    res.send('post request to /cats')
+})
+
+app.get('/dogs', (req, res) => {
+    res.send('woof')
 })
 
 app.listen(5000, () => {
