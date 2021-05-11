bf53d2dd19d8ece77417bce5ae468863a19038d2
Jantzen-E <jantzen.egan@gmail.com>
2021-05-10 22:49:04 -0600

    implemented path parameters in the routes

--- express/firstApp/index.js
+++ express/firstApp/index.js
@@ -9,8 +9,23 @@ const app = express();
 //     res.send('<h1>This is my webpage!</h1>');
 // })
 
+//test all of these with postman
+
 app.get('/', (req, res) => {
     res.send('this is the home page')
+})
+
+//when you write a colon in the path, it is a variable, it is not hard coded
+app.get('/r/:subreddit', (req, res) => {
+    // console.log(req.params)
+    const { subreddit } = req.params;
+    res.send(`<h1>Browsing the ${subreddit} library</h1>`)
+})
+
+app.get('/r/:subreddit/:postId', (req, res) => {
+    // console.log(req.params)
+    const { subreddit, postId } = req.params;
+    res.send(`<h1>viewing post Id: ${ postId } on the ${subreddit} library</h1>`)
 })
 
 app.get('/cats', (req, res) => {
@@ -23,6 +38,12 @@ app.post('/cats', (req, res) => {
 
 app.get('/dogs', (req, res) => {
     res.send('woof')
+})
+
+//this covers all other paths that don't exist, put this at the end of the app.gets or else it will
+//send the res.send string every time
+app.get('*', (req, res) => {
+    res.send(`I don't know that path`)
 })
 
 app.listen(5000, () => {
